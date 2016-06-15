c939ddf5b7f33e2ba8398655a8f8177649adb9cb
imryan <notryancohen@gmail.com>
2016-06-15 16:13:01 -0400

    done

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,25 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *biscuit = [FISDog new];
+    [biscuit makeASound];
+    [biscuit assaultTheMailman];
+    
+    NSLog(@"%@", [(FISPet *)biscuit makeASound]);
+    
+    FISPet *pet = [FISPet new];
+    NSLog(@"%@", [(FISDog *)pet makeASound]);
+    // NSLog(@"%@", [(FISDog *)pet assaultTheMailman]); // unrecognized selector
+    NSLog(@"%@", [biscuit assaultTheMailman]);
+    
+    
     return YES;
 }
 
---
+++ objc-PetCast/FISDog.h
@@ -0,0 +1,17 @@
+//
+//  FISDog.h
+//  objc-PetCast
+//
+//  Created by Ryan Cohen on 6/15/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FISPet.h"
+
+@interface FISDog : FISPet
+
+- (NSString *)makeASound;
+- (NSString *)assaultTheMailman;
+
+@end
